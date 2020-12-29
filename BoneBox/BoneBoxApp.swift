f8eec8983862e4f14afe22a82dcbacb18773e3cb
Kento Shima <sennenn9@gmail.com>
2020-12-30 00:08:05 +0900

    Initial Commit

---
+++ BoneBox/BoneBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoneBoxApp.swift
+//  BoneBox
+//
+//  Created by 島健人 on 2020/12/30.
+//
+
+import SwiftUI
+
+@main
+struct BoneBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoneBox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoneBox
+//
+//  Created by 島健人 on 2020/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
